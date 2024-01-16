280fc6177eb55e801ae3f5989c80e785caf1f5a8
Jonny Vang <jonny.vang@nussbaum-medien.de>
2024-01-16 15:33:42 +0100

    bugfix - fixed layout bug in the drawer menu when resizing the window

--- src/components/MenuDrawer.vue
+++ src/components/MenuDrawer.vue
@@ -12,15 +12,16 @@ function closeDrawer()
 </script>
 
 <template>
-    <div class="drawer h-full flex flex-column min-w-max z-5" :class="{ 'open': isOpen, 'closed': !isOpen }">
-        <div class="flex justify-content-between align-items-center p-2">
-            <MenuLogo />
-            <PrimeButton class="btn-close" icon="bi-x-lg" text rounded aria-label="Close" @click="closeDrawer()" />
+    <div class="drawer h-full flex flex-column justify-content-between min-w-max z-5" :class="{ 'open': isOpen, 'closed': !isOpen }">
+        <div class="flex flex-column">
+            <div class="flex justify-content-between align-items-center p-2">
+                <MenuLogo />
+                <PrimeButton class="btn-close" icon="bi-x-lg" text rounded aria-label="Close" @click="closeDrawer()" />
+            </div>
+            <div class="p-2">
+                <slot></slot>
+            </div>
         </div>
-        <div class="p-2">
-            <slot></slot>
-        </div>
-        <div class="flex-grow-1"></div>
         <div class="p-2">
             <PrimeDivider />
             <div class="text-center">
@@ -29,6 +30,7 @@ function closeDrawer()
                 <span>{{ " " + name }}</span>
             </div>
         </div>
+
     </div>
 </template>
 
