7c9f6f7f94e13136665a55b674f8c230e9e44705
Jonny Vang <jonny.vang@nussbaum-medien.de>
2024-01-29 15:04:44 +0100

    further simplified fetch service

--- src/utils/services/fetch.service.ts
+++ src/utils/services/fetch.service.ts
@@ -1,74 +1,59 @@
 const api = import.meta.env.VITE_API_URL;
 
 export const fetchGET = async (resourceURL: string): Promise<any> => {
-    try {
-        const response = await fetch(api + resourceURL, {
-            method: "GET",
-            headers: {
-                "Content-Type": "application/json"
-            }
-        });
-
-        if (!response.ok) {
-            throw new Error("Network response was not OK - Code: " + response.status);
+    const response = await fetch(api + resourceURL, {
+        method: "GET",
+        headers: {
+            "Content-Type": "application/json"
         }
+    });
 
-        const apiResponse = await response.json();
-
-        if (apiResponse.Success !== true) {
-            throw new Error(apiResponse.Type + ": " + apiResponse.Message);
-        }
-
-        return apiResponse.Response;
-    } catch (error) {
-        console.log("Error catched in service.");
-        throw error;
+    if (!response.ok) {
+        throw new Error("Network response was not OK - Code: " + response.status);
     }
+
+    const apiResponse = await response.json();
+
+    if (apiResponse.Success !== true) {
+        throw new Error(apiResponse.Type + ": " + apiResponse.Message);
+    }
+
+    return apiResponse.Response;
 }
 
 export const fetchPOST = async (resourceURL: string, body: any): Promise<any> => {
-    try {
-        const response = await fetch(api + resourceURL, {
-            method: "POST",
-            headers: {
-                "Content-Type": "application/json"
-            },
-            body: body
-        });
+    const response = await fetch(api + resourceURL, {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json"
+        },
+        body: body
+    });
 
-        if (!response.ok) {
-            throw new Error("Network response was not OK - Code: " + response.status);
-        }
-
-        const apiResponse = await response.json();
-
-        if (apiResponse.Success !== true) {
-            throw new Error(apiResponse.Type + ": " + apiResponse.Message);
-        }
-
-        return apiResponse.Response;
-    } catch (error) {
-        console.log("Error catched in service.");
-        throw error;
+    if (!response.ok) {
+        throw new Error("Network response was not OK - Code: " + response.status);
     }
+
+    const apiResponse = await response.json();
+
+    if (apiResponse.Success !== true) {
+        throw new Error(apiResponse.Type + ": " + apiResponse.Message);
+    }
+
+    return apiResponse.Response;
 }
 
 export const fetchDELETE = async (resourceURL: string): Promise<any> => {
-    try {
-        const response = await fetch(api + resourceURL, {
-            method: "DELETE",
-            headers: {
-                "Content-Type": "application/json"
-            }
-        });
-
-        if (!response.ok) {
-            throw new Error("Network response was not OK - Code: " + response.status);
+    const response = await fetch(api + resourceURL, {
+        method: "DELETE",
+        headers: {
+            "Content-Type": "application/json"
         }
+    });
 
-        return true;
-    } catch (error) {
-        console.log("Error catched in service.");
-        throw error;
+    if (!response.ok) {
+        throw new Error("Network response was not OK - Code: " + response.status);
     }
+
+    return true;
 }
