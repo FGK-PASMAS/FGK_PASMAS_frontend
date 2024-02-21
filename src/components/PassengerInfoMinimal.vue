e6f2e02d9b54447c056f0b4a110ec5aa5341b0e2
J-C-V <jonny-vang@web.de>
2024-02-21 13:08:22 +0100

    added ability to display if passenger weight exceeds seat payload

--- src/components/PassengerInfoMinimal.vue
+++ src/components/PassengerInfoMinimal.vue
@@ -1,16 +1,33 @@
 <script setup lang="ts">
 import type { Passenger } from '@/data/passenger/passenger.interface';
-import type { PropType } from 'vue';
+import { computed, type PropType } from 'vue';
 
-defineProps({
+const props = defineProps({
     passenger: {
         type: Object as PropType<Passenger>,
         required: true
     },
-    seatNumber : {
+    seatNumber: {
         type: Number,
         required: true
+    },
+    seatPayload: {
+        type: Number
     }
+});
+
+const seatOverload = computed(() => {
+    if (!props.seatPayload || props.seatPayload === -1 || !props.passenger.Weight) {
+        return -1;
+    }
+
+    const seatOverload = props.passenger.Weight - props.seatPayload;
+
+    if (seatOverload > 0) {
+        return seatOverload;
+    }
+
+    return 0;
 });
 </script>
 
@@ -19,17 +36,19 @@ defineProps({
         <i class="bi-person-fill" />
         <div class="flex flex-wrap gap-1">
             <div v-if="passenger.LastName || passenger.FirstName" class="flex flex-wrap gap-1">
-                <span v-if="passenger.LastName" class="word-break">{{ passenger.LastName }}</span>
-                <span v-if="passenger.FirstName" class="word-break">{{ passenger.FirstName }}</span>
+                <span v-if="passenger.LastName" class="word-break-all">{{ passenger.LastName }}</span>
+                <span v-if="passenger.FirstName" class="word-break-all">{{ passenger.FirstName }}</span>
             </div>
-            <span v-else>{{ "Passagier #" + seatNumber }}</span>
-            <span>{{ "(" + passenger.Weight + "kg)" }}</span>
+            <span v-else>Passagier #{{ seatNumber }}</span>
+            <span>({{ passenger.Weight }}kg)</span>
+            <div v-if="seatOverload > 0" class="text-red-400">
+                <i class="bi-x" />
+                <span>(+{{ seatOverload }}kg)</span>
+            </div>
+            <i v-else-if="seatOverload !== -1" class="bi-check2 text-primary-400" />
         </div>
     </div>
 </template>
 
 <style scoped lang="scss">
-.word-break {
-    word-break: break-all;
-}
 </style>
