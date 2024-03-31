9ccb12a473c298b22ad1a385b7e5beee1adbcb1e
J-C-V <jonny-vang@web.de>
2024-03-31 01:28:01 +0100

    adjusted debug time frame in config store to just use the current week...

--- src/stores/config.ts
+++ src/stores/config.ts
@@ -9,8 +9,9 @@ export const configStore = defineStore("config", () => {
     // ToDo: Get event interval from somewhere
     function init(): void 
     {
-        eventStartTime.value = DateTime.fromISO("2024-03-20T00:00:00.000000Z");
-        eventEndTime.value = DateTime.fromISO("2024-03-27T20:00:00.000000Z");
+        // Debug time frame
+        eventStartTime.value = DateTime.now().startOf('week');
+        eventEndTime.value = DateTime.now().endOf('week');
     }
 
     return { eventStartTime, eventEndTime, init }
